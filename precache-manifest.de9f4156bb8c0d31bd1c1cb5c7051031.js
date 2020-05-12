f6095b9e0824cedb764e42aeb421cb5660c44139
beyonce knowles <beyonce@thinkful.com>
2020-05-11 20:21:25 -0700

    Updates

---
+++ precache-manifest.de9f4156bb8c0d31bd1c1cb5c7051031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7064328a54bdee5c24419d1292e1ce",
+    "url": "/tim-noteful/index.html"
+  },
+  {
+    "revision": "99e6d3da183834f14a35",
+    "url": "/tim-noteful/static/css/main.5fba27c6.chunk.css"
+  },
+  {
+    "revision": "3834ec88caad2cc44de1",
+    "url": "/tim-noteful/static/js/2.fc08f280.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/tim-noteful/static/js/2.fc08f280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e6d3da183834f14a35",
+    "url": "/tim-noteful/static/js/main.69df514b.chunk.js"
+  },
+  {
+    "revision": "3bb06f60c6a7986a7e13",
+    "url": "/tim-noteful/static/js/runtime-main.d948d494.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.2e9cf34cb360ce4998a3646956e3aec2.js"
+  "/tim-noteful/precache-manifest.de9f4156bb8c0d31bd1c1cb5c7051031.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tim-noteful/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
