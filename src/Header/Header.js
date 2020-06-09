f39216e095e742dd69804cf4814d5b19d00fa1e4
beyonce knowles <beyonce@thinkful.com>
2020-06-08 18:04:15 -0700

    update

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
     return (
         <header>
             <h1>
-                <Link to = {'/'}>Noteful</Link>
+                <Link to = {'/'}>NoteApp</Link>
             </h1>
         </header>
     )
